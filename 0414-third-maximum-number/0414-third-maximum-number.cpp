fa8b3667df904492144445dcbec237d2601697d4
Diagnose <suryansh.vns149@gmail.com>
2026-09-18 21:42:44 +0530

    Time: 0 ms (100%), Space: 12.9 MB (74.1%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+    
+     long long first, second, third; 
+     first = second = third = LLONG_MIN; 
+
+     for(int num:nums)
+     {if(num==first||num==second||num==third)
+     {continue;}
+     
+     if(num>first)
+     {third=second;
+     second=first;
+     first=num;
+     }
+     else if(num>second)
+     {third=second;
+     second=num;}
+     else if(num>third)
+     {third=num;}
+     }
+
+     if(third==LLONG_MIN) return first;
+     else return third;
+    }
+};
