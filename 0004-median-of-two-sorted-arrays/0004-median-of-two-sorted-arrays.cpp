bd06d73999a5f193338b9b06b0cb237f6f18c423
Diagnose <suryansh.vns149@gmail.com>
2026-09-25 21:48:12 +0530

    Time: 3 ms (27.76%), Space: 95.2 MB (41.45%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,24 +3,27 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     int n1 = nums1.size();
     int n2 = nums2.size();
+    int total = n1 + n2;
+    
+    int i = 0, j = 0;
+    int m1 = 0, m2 = 0; 
         
-        vector<int> merge;
-        int i = 0, j = 0;
-        int total_size = n1 + n2;
-        while (i < n1 || j < n2) {
-            if (j >= n2 || (i < n1 && nums1[i] <= nums2[j])) {
-                merge.push_back(nums1[i]);
+    for (int count = 0; count <= total / 2; count++) {
+            m2 = m1; 
+            if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])) 
+            {
+                m1 = nums1[i];
                 i++;
             } 
-            else {
-                merge.push_back(nums2[j]);
+            else 
+            {
+                m1 = nums2[j];
                 j++;
-            }
+            }}
+        if (total % 2 != 0) 
+        {
+            return m1;
         }
-        if (total_size % 2 != 0) {
-            return merge[total_size / 2];
-        } else {
-            return (merge[(total_size / 2) - 1] + merge[total_size / 2]) / 2.0;
-        }
+        return (m1 + m2) / 2.0;
     }
 };
